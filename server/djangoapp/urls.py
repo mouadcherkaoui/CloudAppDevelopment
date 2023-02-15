5a5385c81022e29252441d6ef0b26614dde3106d
mouadcherkaoui <cherkaoui.mouad@live.fr>
2023-02-15 18:03:37 -0500

    adding login/logout/registration logic

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -13,13 +13,16 @@ urlpatterns = [
     path(route='about', view=views.about, name='about'),
 
     # path for contact us view
-   path(route='contact', view=views.contact, name='contact'),
+    path(route='contact', view=views.contact, name='contact'),
 
     # path for registration
+    path(route='registration', view=views.registration, name='registration'),
 
     # path for login
+    path(route='login', view=views.login, name='login'),
 
     # path for logout
+    path(route='logout', view=views.logout, name='logout'),
 
     path(route='', view=views.get_dealerships, name='index'),
 
