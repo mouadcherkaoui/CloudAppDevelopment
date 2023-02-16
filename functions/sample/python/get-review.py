fcc226f5400ab569e90218ad2062d2daecc69ae1
mouadcherkaoui <cherkaoui.mouad@live.fr>
2023-02-16 05:30:43 -0500

    adding function actions logic

---
+++ functions/sample/python/get-review.py
@@ -0,0 +1,39 @@
+#
+#
+# main() will be run when you invoke this action
+#
+# @param Cloud Functions actions accept a single parameter, which must be a JSON object.
+#
+# @return The output of this action, which must be a JSON object.
+#
+#
+import sys
+from ibmcloudant.cloudant_v1 import CloudantV1
+from ibm_cloud_sdk_core.authenticators import IAMAuthenticator
+
+
+def main(param):
+    username = ""
+    if 'username' in param:
+        username = param['username']
+    
+    apiKey = ""
+    if 'apiKey' in param:
+        apiKey = param['apiKey']        
+
+    url = ""
+    if 'url' in param:
+        url = param['url']        
+    
+    authenticator = IAMAuthenticator(apiKey)
+    
+    service = CloudantV1(authenticator=authenticator)
+    
+    service.set_service_url(url)    
+
+    response = service.post_all_docs(
+      db='reviews',
+      include_docs=False,
+    ).get_result()
+    
+    return { 'documents': response }
